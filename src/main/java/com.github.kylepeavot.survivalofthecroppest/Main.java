fd13a8fd2d12a27604c51e56efd66a25584db833
KyIe <kpeavot@hotmail.com>
2020-05-06 19:23:07 +0100

    Example mod: setup basic item with no functionality

--- src/main/java/com.github.kylepeavot.survivalofthecroppest/Main.java
+++ src/main/java/com.github.kylepeavot.survivalofthecroppest/Main.java
@@ -1,13 +1,16 @@
 package com.github.kylepeavot.survivalofthecroppest;
 
 import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(Main.MODID)
 public class Main {
   public static final String MODID = "survival-of-the-croppest";
+  public static final Logger LOGGER = LogManager.getLogger(MODID);
 
   public Main() {
-    //Do nothing
+    LOGGER.debug("Hello world from Main");
   }
 
 }
