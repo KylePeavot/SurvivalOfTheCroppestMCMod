f53b82f15692cdb6a3e3b0c017243378729743e4
KyIe <kpeavot@hotmail.com>
2020-05-06 18:03:04 +0100

    Created mod package and main class. Minecraft recognises the mod as a mod. Woohoo.

---
+++ src/main/java/com.github.kylepeavot.survivalofthecroppest/Main.java
@@ -0,0 +1,13 @@
+package com.github.kylepeavot.survivalofthecroppest;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Main.MODID)
+public class Main {
+  public static final String MODID = "survival-of-the-croppest";
+
+  public Main() {
+    //Do nothing
+  }
+
+}
